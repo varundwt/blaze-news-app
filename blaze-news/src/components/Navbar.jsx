3a5661651105a639caa54ef0693a529ecee1eba9
Varun Pandey <65996450+varundwt@users.noreply.github.com>
2024-08-06 17:51:50 +0530

    "navbar completed"

---
+++ blaze-news/src/components/Navbar.jsx
@@ -0,0 +1,57 @@
+import Logo from "../../public/images/blaze_logo.svg"
+export const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg
+          xmlns=""
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h8m-8 6h16" />
+        </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li><a>Home</a></li>
+        <li>
+          <a>Explore</a>
+          <ul className="p-2">
+            <li><a>Stocks Today</a></li>
+            <li><a>Weather</a></li>
+          </ul>
+        </li>
+        <li><a>Contact Us</a></li>
+      </ul>
+    </div>
+    <img src={Logo} alt="blaze_logo" className="h-10"/>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Home</a></li>
+      <li>
+        <details>
+          <summary>Explore</summary>
+          <ul className="p-2">
+            <li><a>Stocks Today</a></li>
+            <li><a>Weather</a></li>
+          </ul>
+        </details>
+      </li>
+      <li><a>Contact Us</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <a className="btn rounded-xl">Login</a>
+  </div>
+</div>
+  )
+}
