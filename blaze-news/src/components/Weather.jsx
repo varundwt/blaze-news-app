5d180a61160da92abd605e10ec06389918c2d577
Varun Pandey <65996450+varundwt@users.noreply.github.com>
2024-08-14 13:45:44 +0530

    "responsiveness to weather component"

--- blaze-news/src/components/Weather.jsx
+++ blaze-news/src/components/Weather.jsx
@@ -9,31 +9,27 @@
 //http://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=29.7760&lon=79.4268&appid=8237cab4faabbc43f64c9a1cdf207a6a
 
 import { useState } from "react";
-import BrokenClouds from "../../public/images/broken_clouds.svg"
-import ClearSky from "../../public/images/clear_sky.svg"
-import FewClouds from "../../public/images/few_clouds.svg"
-import Mist from "../../public/images/mist.svg"
-import Rain from "../../public/images/rain.svg"
-import ScatteredClouds from "../../public/images/scattered_clouds.svg"
-import ShowerRain from "../../public/images/shower_rain.svg"
-import Snow from "../../public/images/snow.svg"
-import Thunderstorm from "../../public/images/thunderstorm.svg"
+import BrokenClouds from "../../public/images/broken_clouds.svg";
+import ClearSky from "../../public/images/clear_sky.svg";
+import FewClouds from "../../public/images/few_clouds.svg";
+import Mist from "../../public/images/mist.svg";
+import Rain from "../../public/images/rain.svg";
+import Snow from "../../public/images/snow.svg";
+import Thunderstorm from "../../public/images/thunderstorm.svg";
 
 export const Weather = () => {
   const [button, setButton] = useState(false);
   const [weatherData, setWeatherData] = useState(null);
 
-
-  async function getWeather(){
-    const getData = await fetch("https://api.openweathermap.org/data/2.5/weather?q=dwarahat&appid=8237cab4faabbc43f64c9a1cdf207a6a");
+  async function getWeather() {
+    const getData = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=dwarahat&appid=8237cab4faabbc43f64c9a1cdf207a6a"
+    );
     const getDataJSON = await getData.json();
     setWeatherData(getDataJSON);
     setButton(true);
-
-    
   }
 
-  
   return (
     <>
       {!button ? (
@@ -55,7 +51,6 @@ export const Weather = () => {
       ) : (
         <div className="m-24"></div>
       )}
-
       <div className=" text-center">
         <input
           type="text"
@@ -70,43 +65,67 @@ export const Weather = () => {
           Check Weather
         </button>
       </div>
-      {weatherData!=null && (
+      {weatherData != null && (
         <>
-        
-        <div className="border rounded-lg p-5 mx-16 my-5 md:mx-20 lg:mx-36"> 
-        <div className="flex justify-between">
-          <div className="text-3xl font-bold">📍{weatherData.name}, {weatherData.sys.country}</div>
-          <div className="text-xl">{weatherData.coord.lon} {weatherData.coord.lat}</div>          
-        </div>       
-        <div className="flex-row md:flex justify-between px-10">
-        <div className="block ml-auto mr-auto w-1/2 md:ml-0 md:mr-0 md:w-1/3">
-          {weatherData.weather[0].description == 'clear sky' && <img src={ClearSky} alt="" className="w-60 md:w-80"/>}
-          {weatherData.weather[0].description == 'few clouds' && <img src={FewClouds} alt="" className="w-60 md:w-80"/>}
-          {weatherData.weather[0].description == 'scattered clouds' && <img src={ScatteredClouds} alt="" className="w-60 md:w-80"/>}
-          {weatherData.weather[0].description == 'broken clouds' && <img src={BrokenClouds} alt="" className="w-60 md:w-80"/>}
-          {weatherData.weather[0].description == 'shower rain' && <img src={ShowerRain} alt="" className="w-60 md:w-80"/>}
-          {weatherData.weather[0].description == 'rain' && <img src={Rain} alt="" className="w-60 md:w-80"/>}
-          {weatherData.weather[0].description == 'thunderstorm' && <img src={Thunderstorm} alt="" className="w-60 md:w-80"/>}
-          {weatherData.weather[0].description == 'snow' && <img src={Snow} alt="" className="w-60 md:w-80"/>}
-          {weatherData.weather[0].description == 'mist' && <img src={Mist} alt="" className="w-60 md:w-80"/>}
-
-
-        </div>
-        <div className="text-center md:text-left ">
-          <div className=" text-5xl lg:text-8xl md:text-6xl font-bold">{Math.floor((Math.floor(weatherData.main.temp))/10)}°C</div>
-          <div>{weatherData.weather[0].main}</div>
-          <div>{(weatherData.weather[0].description).charAt(0).toUpperCase() + (weatherData.weather[0].description).slice(1)}</div>
-        </div>
-        <div className="text-center md:text-left">        
-        <div>Wind : {weatherData.wind.speed} km/h</div>
-        <div>Wind Gust: {weatherData.wind.gust} km/h</div>
-        <div>Humidity: {weatherData.main.humidity} %</div>
-        <div>Pressure: {weatherData.main.pressure} mB</div>
-        <div>Visibilty: {(weatherData.visibility)/1000} km</div>
-        <div>Cloud Cover: {weatherData.clouds.all} %</div>
-        </div>
-        </div>
-        </div>
+          <div className="border border-blue-500 rounded-lg p-5 mx-8 my-5 md:mx-8 lg:mx-30">
+            <div className="flex justify-between">
+              <div className="text-3xl font-bold ">
+                📍{weatherData.name}, {weatherData.sys.country}
+              </div>
+              <div className="text-xl">
+                {weatherData.coord.lon} {weatherData.coord.lat}
+              </div>
+            </div>
+            <div className="flex-row md:flex justify-between px-10 py-10 md:px-1 md:py-0">
+              <div className="block ml-auto mr-auto w-1/2 md:ml-0 md:mr-0 md:w-1/3 md:py-2">
+                {weatherData.weather[0].main == "Clear" && (
+                  <img src={ClearSky} alt="weather-icon" className="w-60 md:w-80" />
+                )}
+                {weatherData.weather[0].main == "Clouds" && (
+                  <img src={BrokenClouds} alt="weather-icon" className="w-60 md:w-80 mt-auto mb-auto" />
+                )}
+                {weatherData.weather[0].main == "Drizzle" && (
+                  <img src={FewClouds} alt="weather-icon" className="w-60 md:w-80" />
+                )}
+                {weatherData.weather[0].main == "Rain" && (
+                  <img src={Rain} alt="weather-icon" className="w-60 md:w-80" />
+                )}
+                {weatherData.weather[0].main == "Thunderstorm" && (
+                  <img src={Thunderstorm} alt="weather-icon" className="w-60 md:w-80" />
+                )}
+                {weatherData.weather[0].main == "Snow" && (
+                  <img src={Snow} alt="weather-icon" className="w-60 md:w-80" />
+                )}
+                {weatherData.weather[0].main == "Atmosphere" && (
+                  <img src={Mist} alt="weather-icon" className="w-60 md:w-80" />
+                )}
+              </div>
+              <div className="text-center md:text-left md:mt-auto md:mb-auto">
+                <div className="text-blue-400 text-5xl font-bold lg:text-8xl md:text-6xl md:h-20">
+                  {Math.floor(Math.floor(weatherData.main.temp) / 10)}°C
+                </div>
+                <div className="text-3xl font-bold py-2 md:py-0 md:pb-0">{weatherData.weather[0].main}</div>
+                <div className="text-xl">
+                  {weatherData.weather[0].description.charAt(0).toUpperCase() +
+                    weatherData.weather[0].description.slice(1)}
+                </div>
+              </div>
+              <div className="py-10 md:py-12 md:mt-auto md:mb-auto">
+              <div className="text-center justify-between text-lg py-2 md:text-center flex md:flex-wrap md:text-sm">
+                <div className="px-1 md:px-3">Wind <div>{weatherData.wind.speed} km/h</div> </div>
+                <div className="px-1 md:px-3">Gust <div>{weatherData.wind.gust} km/h</div> </div>
+                <div className="px-1 md:px-3">Humidity <div>{weatherData.main.humidity} %</div></div>                
+              </div>
+              <div className="text-center justify-between text-lg py-2 md:text-center flex md:flex-wrap md:text-sm">
+                
+                <div className="px-1 md:px-3">Pressure <div>{weatherData.main.pressure} mB</div></div>
+                <div className="px-1 md:px-3">Cloud Cover <div>{weatherData.clouds.all} %</div></div>
+                <div className="px-1 md:px-3">Visibilty <div>{weatherData.visibility / 1000} km</div></div>
+                
+              </div>
+              </div>
+            </div>
+          </div>
         </>
       )}
     </>
